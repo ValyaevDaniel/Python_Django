ec79e86d6b352e559e4c1bdb2bb84e3b875a24a7
Hidex <123456vdv@mail.ru>
2021-10-19 19:32:54 +0300

    blok_1: (laba_1;laba_2)

--- Python_Standart/main.py
+++ Python_Standart/main.py
@@ -1,3 +1,12 @@
+RandomNumber = int(input("Введите произвольное число: "))
+BorderNumber = int(input("Введите пограничное число: "))
+
+if RandomNumber < BorderNumber:
+    print("Произвольное число меньше пограничного")
+elif RandomNumber/3 >= BorderNumber:
+    print("Произвольное число больше пограничного в 3 раза")
+else:
+    print("Произвольное число больше пограничного")
 
 
 
